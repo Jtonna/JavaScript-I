d6ee24ad1d3cf62ca7921faadbd1eb80b7b04e81
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-04-01 14:00:55 -0700

    Update objects.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -89,6 +89,7 @@ console.log("It appears that", internObj5.name, "identifies as a", internObj5.ge
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
+console.log(internObj1.speak("Hello, my name is Kennan!"));
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
